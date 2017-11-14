03bcecd57c6dd0956f9aae6c289e4c61da71ffaa
Anshul Verma <anshulverma96@gmail.com>
2017-11-15 00:49:05 +0530

    add_remove_completed

--- js/controller.js
+++ js/controller.js
@@ -11,8 +11,18 @@ myApp.controller('HomeCtrl', function($scope, $state){
 
 .controller('AboutCtrl', function($scope,$state){
 	$scope.values = [];
+	$scope.ticks = [];
 	$scope.toDoList = function(){
-		console.log($scope.user);
-		 $scope.values.push($scope.user.l);
+		$scope.values.push($scope.user.l);
+		$scope.user.l = '';
+	}
+	$scope.checkList = function(){
+		$scope.ticks.push({check:'done-true' , done:false});
+
+	}
+	$scope.removeList = function(index){
+		$scope.values.splice(index,1);
+		
+		 
 	}
 })
